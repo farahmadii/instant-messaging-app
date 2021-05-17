e93e190d6ea94631c60795680e39ed1174f54d18
farahmadii <33867973+farahmadii@users.noreply.github.com>
2021-05-17 15:07:43 +0200

    removing the exclusion to be able to use spring security

--- src/main/java/com/farzan/chat/ChatApplication.java
+++ src/main/java/com/farzan/chat/ChatApplication.java
@@ -1,12 +1,10 @@
 package com.farzan.chat;
 
-import com.farzan.chat.service.MessageService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication //(exclude = {SecurityAutoConfiguration.class})
 public class ChatApplication {
 
 	public static void main(String[] args) {
